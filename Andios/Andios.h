d772946a1570aecb4edf53e46e67b4ea86b3c32f
typark <dogameadmin@gmail.com>
2018-03-11 15:00:42 +0900

    Initial commit

---
+++ Andios/Andios.h
@@ -0,0 +1,19 @@
+//
+//  Andios.h
+//  Andios
+//
+//  Created by typark on 2018. 3. 11..
+//  Copyright © 2018년 typark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Andios.
+FOUNDATION_EXPORT double AndiosVersionNumber;
+
+//! Project version string for Andios.
+FOUNDATION_EXPORT const unsigned char AndiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Andios/PublicHeader.h>
+
+
